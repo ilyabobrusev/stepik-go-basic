e0a7354774b734f4bb073b8fef1f3987a6863819
ilyabobrusev@nb01 <ilyabobrusev@dom.com>
2024-12-19 01:10:16 +0300

    Add progress mod 2

--- 2/2_6/main.go
+++ 2/2_6/main.go
@@ -123,3 +123,174 @@ func main() {
 
 //
 
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main(){
+    var n float64 = -9
+    fmt.Println(math.Abs(n)) // берет модуль числа и выводит на экран число 9
+}
+
+//
+
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    var n float64 = 8
+    fmt.Println(math.Sqrt(n))
+}
+
+//
+
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    var n float64 = 8
+    fmt.Println(math.Pow(n, 2))
+}
+
+//
+
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    var n float64 = 8
+    fmt.Println(math.Max(n, 4)) // выводит на экран число 8
+    fmt.Println(math.Min(n, 4)) // выводит на экран число 4
+}
+
+//
+
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    var n float64
+    fmt.Scan(&n)
+    fmt.Println(n / math.Pow(2, 13))
+}
+
+//
+
+
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    var a, b float64
+    _, _ = fmt.Scan(&a, &b)
+    fmt.Println(math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2)) + (a + b))
+}
+
+//
+
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    var a, b, c, d float64
+    _, _ = fmt.Scan(&a, &b, &c, &d)
+    fmt.Println(math.Sqrt(math.Pow(a - c, 2) + math.Pow(b - d, 2)))
+}
+
+//
+
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    var a, b, c, d float64
+    _, _ = fmt.Scan(&a, &b, &c, &d)
+    fmt.Println(math.Abs((a-c)) + math.Abs((b-d)))
+}
+
+//
+
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var a, b, c int
+    _, _ = fmt.Scan(&a, &b, &c)
+    fmt.Println((a+b+c), (a*b*c))
+}
+
+//
+
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var n int
+    _, _ = fmt.Scan(&n)
+    fmt.Println(n / 100 % 10)
+}
+
+//
+
+package main
+
+import (
+    "fmt"
+)
+func main() {
+    var a int
+    _, _ = fmt.Scan(&a)
+    fmt.Println((a%10)+(a/10%10)+(a/100%10))
+}
+
+//
+
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    var a, b float64
+    _, _ = fmt.Scan(&a, &b)
+    fmt.Println(math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2)))
+}
