f840a2c796faf1a2eeb36143b075c6b1d25b0fa3
ilyabobrusev@nb01 <ilyabobrusev@dom.com>
2024-12-27 01:01:10 +0300

    Add progress mod 4.1

--- 4/4.1/main.go
+++ 4/4.1/main.go
@@ -9,3 +9,86 @@ func main() {
 }
 
 //
+
+package main
+
+import "fmt"
+
+func main() {
+    for i := 1; i <= 100; i++ {
+        if i%2 == 0 {
+            fmt.Println(i)
+        }
+    }
+}
+
+//
+package main
+
+import "fmt"
+
+func main() {
+    for i := 2; i <= 100; i = i + 2 {
+        fmt.Println(i)
+    }
+}
+
+//
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+
+    for i := a; i <= b; i++ {
+        if i%2 == 0 {
+            fmt.Println(i)
+        }
+    }
+}
+
+//
+package main
+
+import "fmt"
+
+func main() {
+	var x int
+	_, _ = fmt.Scan(&x)
+
+	for i := 1; i <= x; i++ {
+		if x%i == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+//
+package main
+
+import "fmt"
+
+func main() {
+    var a, b int 
+    fmt.Scan(&a, &b)
+    for ; b >= a; b-- {
+        fmt.Println(b)
+    } 
+}
+
+//
+package main
+
+import "fmt"
+
+func main() {
+	var x int
+	_, _ = fmt.Scan(&x)
+
+	for i := 1; i <= x; i++ {
+		fmt.Println(i*i)
+	}
+}
